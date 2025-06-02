ec6d0760a867eaec14959419dd70f6f0e46270cf
Kareem-Bizreh <kareembezreh@gmail.com>
2025-06-02 11:32:09 +0300

    first commit

---
+++ dashboard-real-estate-office/src/App.jsx
@@ -0,0 +1,11 @@
+// import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-lg">r e o</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ dashboard-service-provider/src/App.jsx
@@ -0,0 +1,11 @@
+// import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-lg">service p</h1>
+    </>
+  );
+}
+
+export default App;
